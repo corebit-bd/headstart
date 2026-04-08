3bd0e11699e68b37fd929411dc9ae5dab869614a
Jeet Khondker <jeetzhkhondker@gmail.com>
2026-04-09 08:39:08 +0900

    chore (fix) : Updated 'eslint.config.mjs' File to disable ReactJS Rules for Storybook Configuration Files in Frontend Module

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,19 @@ const eslintConfig = [
     // Replacement for globalIgnores
     ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
+  // Storybook Plugin
+  {
+    files: ["**/*.stories.ts", "**/*.stories.tsx"],
+    ...storybook.configs["flat/recommended"],
+  },
+  // Disable ReactJS Rules for Storybook Configuration Files
+  {
+    files: [".storybook/**/*.ts", ".storybook/**/*.tsx"],
+    rules: {
+      "react/display-name": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
